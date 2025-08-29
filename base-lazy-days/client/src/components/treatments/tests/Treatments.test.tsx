7205fbc53a09f22b45d037080537a3e6bc6e6cda
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2025-08-29 12:31:05 -0400

    adds ut for simulating error in AllStaff component

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,11 @@
 import { Treatments } from "../Treatments";
 
-import { render } from "@/test-utils";
+import { render, screen } from "@/test-utils";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
   render(<Treatments />);
+  const treatmentTitles = await screen.findAllByRole("heading", {
+    name: /massage|facial|scrub/i,
+  });
+  expect(treatmentTitles).toHaveLength(3);
 });
